ab383d9a86f84db2cded7ba1684ee683d0559351
szzdmj <chuanfuyin001@gmail.com>
2025-08-02 07:18:41 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,19 @@
+// worker.js
+import phpWasm from 'cloudflare
+/php.wasm';
+import indexPhp from 'Static_Creation/public/index.php';
+
+export default {
+  async fetch(request, env, ctx) {
+    const { instance } = await WebAssembly.instantiate(phpWasm, {
+      env: {
+        // 这里可以根据需要添加导入的函数，如 memory, table, imports 等
+      }
+    });
+
+    // 简单示例：返回 index.php 文件内容
+    return new Response(indexPhp, {
+      headers: { 'Content-Type': 'text/html' }
+    });
+  }
+};
