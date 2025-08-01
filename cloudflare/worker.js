5ffeb8485767b8065f509c729655f8d1b3f15922
szzdmj <chuanfuyin001@gmail.com>
2025-08-02 05:27:51 +0800

    Update worker.js

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -1,1 +1,29 @@
-test
+import { PhpWebWorker } from '@php-wasm/web';
+
+const BOOTSTRAP_FILE = 'index.php';
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    const php = new PhpWebWorker({
+      wasmBinaryPath: new URL('php.wasm', import.meta.url).toString(),
+    });
+
+    const file = url.pathname === '/' ? BOOTSTRAP_FILE : decodeURIComponent(url.pathname.slice(1));
+
+    const response = await php.run({
+      documentRoot: '/',
+      scriptName: `/${file}`,
+      requestUrl: request.url,
+      method: request.method,
+      headers: Object.fromEntries(request.headers),
+      body: request.body,
+      files: {},
+    });
+
+    return new Response(response.body, {
+      status: response.statusCode,
+      headers: response.headers,
+    });
+  },
+};
