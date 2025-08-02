b99368181453c4967c461598a2f5f89284f7fa91
szzdmj <chuanfuyin001@gmail.com>
2025-08-02 22:57:29 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
-import phpWasm from 'cloudflare/php.wasm';
+import phpWasm from 'phpWasm';
 
 export default {
   async fetch(request, env, ctx) {
     const wasmModule = await WebAssembly.compile(phpWasm);
-    const instance = await WebAssembly.instantiate(wasmModule, { /* empty imports for now */ });
+    const instance = await WebAssembly.instantiate(wasmModule, { /* empty imports */ });
 
     return new Response("WASM loaded & instantiated!", {
       headers: { 'content-type': 'text/plain' }
