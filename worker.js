3c6f1ef83620cc10fc3cb7cda54877d2d40765bf
szzdmj <chuanfuyin001@gmail.com>
2025-08-02 22:48:11 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,22 +1,7 @@
 export default {
   async fetch(request, env, ctx) {
-    // Load wasm binary
-    const wasmResponse = await fetch(new URL('cloudflare/php.wasm', import.meta.url));
-    const wasmBinary = await wasmResponse.arrayBuffer();
-    const wasmModule = await WebAssembly.compile(wasmBinary);
-
-    // Load index.php content (from KV or URL)
-    const phpResponse = await fetch(new URL('Static_Creation/public/index.php', import.meta.url));
-    const indexPhp = await phpResponse.text();
-
-    // Initialize wasm instance (imports need to match Embed API)
-    const instance = await WebAssembly.instantiate(wasmModule, {/* imports */});
-
-    // Run PHP code (runPhp 是 wasm 导出函数)
-    const result = instance.exports.runPhp(indexPhp);
-
-    return new Response(result, {
-      headers: { 'content-type': 'text/html' }
+    return new Response("PHP wasm Worker is alive!", {
+      headers: { 'content-type': 'text/plain' }
     });
   }
 };
