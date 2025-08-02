9349422a859652347e85748e3fa3a674f848053d
szzdmj <chuanfuyin001@gmail.com>
2025-08-02 23:13:45 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,16 +1,11 @@
 import phpWasm from './cloudflare/php.wasm';
 
-// index.php 不用 import，改成 fetch 动态读取
 export default {
   async fetch(request, env, ctx) {
     const wasmModule = await WebAssembly.compile(phpWasm);
     const instance = await WebAssembly.instantiate(wasmModule, {});
 
-    // 动态读取 index.php 内容
-    const phpResponse = await fetch(new URL('./Static_Creation/public/index.php', import.meta.url));
-    const indexPhp = await phpResponse.text();
-
-    return new Response("WASM loaded & index.php read!", {
+    return new Response("WASM loaded & instantiated OK!", {
       headers: { 'content-type': 'text/plain' }
     });
   }
