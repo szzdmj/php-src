928f127a85e3f6a9d0fe8df143d9dd7b10961b13
szzdmj <chuanfuyin001@gmail.com>
2025-08-02 23:17:40 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,10 @@
-import phpWasm from './cloudflare/php.wasm';
-
+const wasmCode = new Uint8Array([
+  0x00,0x61,0x73,0x6d,0x01,0x00,0x00,0x00, // Minimal valid WASM binary
+]);
 export default {
-  async fetch(request, env, ctx) {
-    const wasmModule = await WebAssembly.compile(phpWasm);
-    const instance = await WebAssembly.instantiate(wasmModule, {});
-
-    return new Response("WASM loaded & instantiated OK!", {
-      headers: { 'content-type': 'text/plain' }
-    });
+  async fetch(request) {
+    const module = await WebAssembly.compile(wasmCode);
+    const instance = await WebAssembly.instantiate(module);
+    return new Response("Minimal wasm OK!", { headers: { 'content-type': 'text/plain' } });
   }
 };
