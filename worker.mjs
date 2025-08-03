c53a4cb3a7dcd0fe19e40eb3bc17c6eb97d6cea0
szzdmj <chuanfuyin001@gmail.com>
2025-08-03 14:44:57 +0800

    Update worker.mjs

--- worker.mjs
+++ worker.mjs
@@ -1,12 +1,8 @@
+import PHP_WASM from './php.wasm';
+
 export default {
   async fetch(request, env, ctx) {
-    const wasmResponse = await fetch(new URL('./php.wasm', import.meta.url));
-    const wasmArrayBuffer = await wasmResponse.arrayBuffer();
-
-    // 只用 instantiate，不要 compile 再 instantiate
-    const { instance } = await WebAssembly.instantiate(wasmArrayBuffer, {});
-
-    // 这里可以调用 wasm 导出的方法
+    const { instance } = await WebAssembly.instantiate(PHP_WASM, {});
     return new Response("WASM Loaded Successfully", { status: 200 });
   }
 };
