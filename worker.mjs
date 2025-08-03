7138010436f11bb13842ad97558e264ade48a17d
szzdmj <chuanfuyin001@gmail.com>
2025-08-03 17:19:34 +0800

    Update worker.mjs

--- worker.mjs
+++ worker.mjs
@@ -1,16 +1,12 @@
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
+    // 读取 wasm 文件
+    const wasmModuleResponse = await env.ASSETS.fetch('php.wasm');
+    const wasmBuffer = await wasmModuleResponse.arrayBuffer();
 
-    if (url.pathname === '/php.wasm') {
-      // 读取 cloudflare/php.wasm 文件
-      const asset = await env.ASSETS.fetch('php.wasm');
-      return new Response(asset.body, {
-        headers: { 'Content-Type': 'application/wasm' }
-      });
-    }
+    // 实例化 WebAssembly
+    const wasmInstance = await WebAssembly.instantiate(wasmBuffer, {});
 
-    // 默认返回
-    return new Response('Worker is alive');
+    return new Response('WASM Loaded. Instance created.');
   }
 };
