3be6187265b882f468b6fd6d779d2254704a994a
szzdmj <chuanfuyin001@gmail.com>
2025-08-03 17:25:19 +0800

    Update worker.mjs

--- worker.mjs
+++ worker.mjs
@@ -1,12 +1,16 @@
 export default {
   async fetch(request, env, ctx) {
-    // 读取 wasm 文件
-    const wasmModuleResponse = await env.ASSETS.fetch('php.wasm');
-    const wasmBuffer = await wasmModuleResponse.arrayBuffer();
+    try {
+      // 读取 php.wasm 文件
+      const wasmResponse = await env.ASSETS.fetch('php.wasm');
+      const wasmBuffer = await wasmResponse.arrayBuffer();
 
-    // 实例化 WebAssembly
-    const wasmInstance = await WebAssembly.instantiate(wasmBuffer, {});
+      // 实例化 WebAssembly
+      const { instance } = await WebAssembly.instantiate(wasmBuffer, {});
 
-    return new Response('WASM Loaded. Instance created.');
+      return new Response('WASM Loaded. Instance created.');
+    } catch (e) {
+      return new Response(`Error: ${e.message}`, { status: 500 });
+    }
   }
-};
+}
