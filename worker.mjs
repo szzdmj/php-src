e0a7dc10f5fb480cc54875bd938b34340af57454
szzdmj <chuanfuyin001@gmail.com>
2025-08-03 16:04:08 +0800

    Update worker.mjs

--- worker.mjs
+++ worker.mjs
@@ -1,8 +1,5 @@
 export default {
   async fetch(request) {
-    const wasmUrl = new URL('./php.wasm', import.meta.url).toString();
-    const wasmModule = await fetch(wasmUrl).then(res => res.arrayBuffer());
-    const { instance } = await WebAssembly.instantiate(wasmModule, {});
-    return new Response('WASM Loaded');
+    return new Response("Worker is alive");
   }
 }
